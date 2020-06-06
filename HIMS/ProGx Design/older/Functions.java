c2b1b85f700276608bb3b48c0396fb17a798b372
nassoro <medydullah01@gmail.com>
2020-06-06 10:39:53 +0300

    backup

---
+++ HIMS/ProGx Design/older/Functions.java
@@ -0,0 +1,38 @@
+1.0 HealthCareAdmin
+->register health care provider
+->add employees
+->view employees
+->edit employees
+->disable employees
+
+
+
+2.0 HealthCareEmployee
+[2.1 Doctor]
+->record symptoms
+->recommend tests
+->prescibe medicines
+
+
+[2.1 Reception]
+->create a visit form
+->verify tests payments
+
+
+[2.2 Nurse]
+->fill basic information
+
+
+
+[2.3 Lab Technician]
+->record test results
+
+
+[2.4 Pharmacist]
+->verify issued medicine
+
+
+
+[All User]
+->reset passowrd
+->change password
